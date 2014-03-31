7e0a93d725c20a3bec7500f775786d4e3149db5e
Rodrigo Lopez <rodlopezesca@outlook.com>
2014-03-31 16:30:40 -0600

    inicio app
    cargando los primeros archivos

---
+++ app/app.js
@@ -0,0 +1,21 @@
+Ext.application({
+    name: 'app',
+	
+	appFolder : 'js/app',
+	
+	
+	controllers: ['Main'],
+	/*models: [],
+	stores:[],
+	
+	
+	*/
+	views:['MenuTree'],
+	
+    launch: function() {
+	
+	console.log('launch!!!');
+	
+        
+    }
+});
